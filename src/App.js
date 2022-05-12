b43622bc65cb59346170d17c5509c9cdebd9a0ca
Hansaj Sharma <hansajsharma123@hmail.com>
2022-05-13 02:25:55 +0530

    lesson 156

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// pages
+import Dashboard from './pages/dashboard/Dashboard'
+import Create from './pages/create/Create'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Project from './pages/project/Project'
+
 import './App.css'
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-
-    </div>
+      <BrowserRouter>
+        <Sidebar />
+        <div className="container">
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/create' element={<Create />} />
+            <Route path='/project/:id' element={<Project />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div >
   );
 }
 
 export default App
+
+/*
+pages
+
+dashboard 
+login
+signup
+create
+project 
+
+*/
---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBnuQv4PsUT5UGJZlfJ9O7PoSB9ECm4aJs",
+    authDomain: "projesite-579cf.firebaseapp.com",
+    projectId: "projesite-579cf",
+    storageBucket: "projesite-579cf.appspot.com",
+    messagingSenderId: "184626361629",
+    appId: "1:184626361629:web:14776d33fb8764b99a5df4"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp 
+const timestamp = firebase.firestore.Timestamp
+
+export { projectAuth, projectFirestore }
